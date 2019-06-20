5bf1fc2ea6ee45bcd5ed4d5b50e7d93016edd945
Kamil Chlebek <kamil.chlebek94@gmail.com>
2019-06-20 11:55:44 +0200

    Create two sample apps

---
+++ apps/app-two/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'webstorm-karma-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-two';
+}
